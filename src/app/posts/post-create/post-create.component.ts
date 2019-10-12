62b9461cfbe054d8a1c83de56a3435ca9030ee5f
Owais Ibrahim <owaisibrahim99@gmail.com>
2019-10-13 00:29:33 +0500

    Adding an Image Preview

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -14,6 +14,7 @@ export class PostCreateComponent implements OnInit {
   private postId: string;
   post: Post;
   form: FormGroup;
+  imagePreview: string;
   isLoading = false;
 
   constructor(public postsService: PostsService, public router: ActivatedRoute) {}
@@ -50,8 +51,12 @@ export class PostCreateComponent implements OnInit {
     const file = (event.target as HTMLInputElement).files[0];
     this.form.patchValue({image: file});
     this.form.get('image').updateValueAndValidity();
-    console.log(file);
-    console.log(this.form);
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
