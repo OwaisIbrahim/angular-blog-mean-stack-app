7c536ab34fc1f5d857f2f6ed07d4094971ada9dd
Owais Ibrahim <owaisibrahim99@gmail.com>
2019-10-12 12:23:15 +0500

    4.9 Backend Route Reorganize

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+const PostComponent = require('./routes/post');
 
 const app = express();
 
@@ -23,7 +22,6 @@ mongoose.connect(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -35,63 +33,8 @@ app.use((req, res, next) => {
       "GET, POST, PATCH, PUT, DELETE, OPTIONS"
     );
   next();
-})
-
-app.post("/api/posts", (req, res, next) => {
-  var post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(result => {
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: result._id
-    });
-  });
 });
 
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.params.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({_id: req.params.id}, post).then((result) => {
-    console.log(result);
-    res.status(200).json({
-      message: "Post updated successfully"
-    });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  //return all entries
-  Post.find()
-    .then(documents => {
-      res.status(200).json({
-        message: 'posts fetched succussfully',
-        posts: documents
-      });
-    });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if(post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({message: 'Post not found'})
-    }
-  })
-})
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({_id: req.params.id}).then(result => {
-    console.log(result);
-    res.status(200).json({
-      message: 'Post deleted successfully'
-    });
-  });
-});
+app.use('/api/posts', PostComponent)
 
 module.exports = app;
